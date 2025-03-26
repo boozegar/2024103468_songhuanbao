3f57426950a73cb5d27b23bbc595112a90f31e4a
boozegar <2654360399@qq.com>
2025-03-26 13:21:00 +0800

    update

--- work2/w2.py
+++ work2/w2.py
@@ -1,38 +1,84 @@
 import random
 
-"""
-num:
-**kwargs:
-"""
-def random_fun(num, **kwargs):
-    result = list()
-    for _ in range(0, num):
-        element = list()
-        for key, value in kwargs.items():
-            if key == "int":
-                it = iter(value['data_range'])
-                tmp = random.randint(next(it), next(it))
-            elif key == "float":
-                it = iter(value['data_range'])
-                tmp = random.uniform(next(it), next(it))
-            elif key == "str":
-                tmp = ''.join(random.SystemRandom().choice(value['data_range']) for _ in range(value['len']))
-            else:
-                break
-            element.append(tmp)
-        result.append(tmp)
-    return result
+import string
 
 
+def generate_random_string(length):
+    all_characters = string.ascii_letters + string.digits + string.punctuation
+    random_string = ''.join(random.choice(all_characters) for _ in range(
+        length))
+    return random_string
+
+
+def generate_data(**kwargs):
+    ds = kwargs['collection']['constructor']()
+    data_type = kwargs['type']
+    add = kwargs['collection']['add']
+    if data_type == 'sub_items':
+        for si in kwargs['sub_items']:
+            add(ds, generate_data(**si))
+    else:
+        for _ in range(kwargs['collection']['len']):
+            add(ds, kwargs['generator']())
+    return ds
 
 
 if __name__ == '__main__':
-    # test_case = [{
-    #     "num":5,
-    #     "kwargs"
-    # }]
-    print(random_fun(5, **{
-        "int": {
-            "data_range": [1, 10]
-        }
-    }))
+    kw = {
+        "type": "sub_items",
+        "collection": {
+            "constructor": list,
+            "add": lambda ds, item: ds.append(item)
+        },
+        "sub_items": [
+            {
+                "type": "sub_items",
+                "sub_items": [
+                    {
+                        "type": "generator",
+                        "collection": {
+                            "constructor": list,
+                            "add": lambda ds, item: ds.append(item),
+                            "len": 5
+                        },
+                        "generator": lambda: ((random.randint(0, 255),
+                                              random.randint(0, 255),
+                                              random.randint(0, 255))),
+                    }
+                ],
+                "collection": {
+                    "constructor": list,
+                    "add": lambda ds, item: ds.append(item),
+                },
+            },
+            {
+                "type": "generator",
+                "generator": lambda: random.randint(0, 10),
+                "collection": {
+                    "constructor": list,
+                    "add": lambda ds, item: ds.append(item),
+                    "len": 10,
+                },
+            },
+            {
+                "type": "generator",
+                "generator": lambda: random.uniform(0., 10.),
+                "collection": {
+                    "constructor": list,
+                    "add": lambda ds, item: ds.append(item),
+                    "len": 10,
+                },
+            },
+            {
+                "type": "generator",
+                "generator": lambda: generate_random_string(10),
+                "collection": {
+                    "constructor": list,
+                    "add": lambda ds, item: ds.append(item),
+                    "len": 10,
+                },
+            }
+        ],
+
+    }
+    print(generate_data(**kw))
